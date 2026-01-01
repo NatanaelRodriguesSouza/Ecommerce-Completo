aad8a95a1c9edfc3bc6295f0c24bbf5c74207292
NatanaelRodriguesSouza <natanaelrodriguessouza13@gmail.com>
2026-01-01 15:27:40 -0300

    Primeiro teste Realizado : Entidade MarcaProduto criada

---
+++ src/main/java/Ecommerce_Completo/model/MarcaProduto.java
@@ -0,0 +1,34 @@
+package Ecommerce_Completo.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_Marca_Produto")
+@SequenceGenerator(name = "seq_marca_produto", sequenceName = "seq_marca_produto", allocationSize = 1, initialValue = 1)
+public class MarcaProduto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto")
+    private Long id;
+
+    private String nomeDesc;
+
+    public MarcaProduto() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomeDesc() {
+        return nomeDesc;
+    }
+
+    public void setNomeDesc(String nomeDesc) {
+        this.nomeDesc = nomeDesc;
+    }
+}
