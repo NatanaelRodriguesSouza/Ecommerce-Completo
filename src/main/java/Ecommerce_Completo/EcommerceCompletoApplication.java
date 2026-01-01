04b7a4a6ea3e1b40456e8048b4b97a0fefa944dc
NatanaelRodriguesSouza <natanaelrodriguessouza13@gmail.com>
2026-01-01 14:59:08 -0300

    Projeto Iniciado

---
+++ src/main/java/Ecommerce_Completo/EcommerceCompletoApplication.java
@@ -0,0 +1,13 @@
+package Ecommerce_Completo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceCompletoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceCompletoApplication.class, args);
+	}
+
+}
