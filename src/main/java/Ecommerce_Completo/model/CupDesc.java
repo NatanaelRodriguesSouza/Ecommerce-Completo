b126bea63b3646134b3047c3356c880fba3eb1a6
NatanaelRodriguesSouza <natanaelrodriguessouza13@gmail.com>
2026-01-03 17:40:56 -0300

    Entidade CupDesc foi criado

---
+++ src/main/java/Ecommerce_Completo/model/CupDesc.java
@@ -0,0 +1,72 @@
+package Ecommerce_Completo.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_cupDesc")
+@SequenceGenerator(name = "seq_cupDesc" , sequenceName = "seq_cupDesc",allocationSize = 1,initialValue = 1)
+public class CupDesc {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_cupDesc")
+    private Long id;
+    @Column(nullable = false)
+    private String codDesc;
+
+    private BigDecimal valorRealDesc;
+
+    private BigDecimal valorPorcentDesc;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.DATE)
+    private Date dataValidadeCupom;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCodDesc() {
+        return codDesc;
+    }
+
+    public void setCodDesc(String codDesc) {
+        this.codDesc = codDesc;
+    }
+
+    public BigDecimal getValorRealDesc() {
+        return valorRealDesc;
+    }
+
+    public void setValorRealDesc(BigDecimal valorRealDesc) {
+        this.valorRealDesc = valorRealDesc;
+    }
+
+    public BigDecimal getValorPorcentDesc() {
+        return valorPorcentDesc;
+    }
+
+    public void setValorPorcentDesc(BigDecimal valorPorcentDesc) {
+        this.valorPorcentDesc = valorPorcentDesc;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        CupDesc cupDesc = (CupDesc) o;
+        return Objects.equals(id, cupDesc.id) && Objects.equals(codDesc, cupDesc.codDesc) && Objects.equals(valorRealDesc, cupDesc.valorRealDesc) && Objects.equals(valorPorcentDesc, cupDesc.valorPorcentDesc) && Objects.equals(dataValidadeCupom, cupDesc.dataValidadeCupom);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, codDesc, valorRealDesc, valorPorcentDesc, dataValidadeCupom);
+    }
+}
